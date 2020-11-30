250ac432f8f037f1a7e6e2fee451355debdb9ddb
westwashin <zjaksu@gmail.com>
2020-11-30 23:42:37 +0700

    Add Contact Form

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php 
+    $result = "";
+    $error  = "";
+if(isset($_POST['submit']))
+{
+    require 'phpmailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+    //smtp settings
+    $mail->isSMTP(); // send as HTML
+    $mail->Host = "smtp.gmail.com"; // SMTP servers
+    $mail->SMTPAuth = true; // turn on SMTP authentication
+    $mail->Username = "zjaksu@gmail.com"; // Your mail
+    $mail->Password = 'Insomni@132'; // Your password mail
+    $mail->Port = 587; //specify SMTP Port
+    $mail->SMTPSecure = 'tls';                               
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress('zjaksu@gmail.com');
+    $mail->addReplyTo($_POST['email'],$_POST['name']);
+    $mail->isHTML(true);
+    $mail->Subject='Form Submission:' .$_POST['subject'];
+    $mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h3>';
+    if(!$mail->send())
+    {
+        $error = "Something went worng. Please try again.";
+    }
+    else 
+    {
+        $result="Thanks\t" .$_POST['name']. " for contacting us.";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -150,17 +184,35 @@
 		<section id="contact">
 			<div class="container">
 				<h3>Contact</h3>
-				<div class="col-1">
-					<a href="mailto:zjaksu@gmail.com"><i class="far fa-envelope"> zjaksu@gmail.com</i></a>
-				</div>
-				
-				<div class="col-1">
-					<a href="https://github.com/westwashin"><i class="fab fa-github"> westwashin</i></a>
-				</div>
-				
-				<div class="col-1">
-					<a href="https://www.linkedin.com/in/syuja-zhafran-777b331ba/"><i class="fab fa-linkedin"> Syuja Zhafran</i></a>
-				</div>
+				<h2 class="text-center text-white"> <?=$result; ?></h2>
+                    <h2 class="text-center text-danger"> <?=$error; ?></h2>
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label for="inputName">Name</label>
+                                    <input type="text" class="form-control" id="Name" name="name" placeholder="Enter Full" required>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="inputEmail">Email</label>
+                                <input type="email" class="form-control" id="Email" name="email" placeholder="Enter Email" required>
+                            </div>
+                        </div>
+                    </div>            
+                    <div class="form-group">
+                        <label for="inputSubject">Subject</label>
+                        <input type="text" class="form-control" id="Subject"​ name="subject" placeholder="Enter Subject" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputMessage">Message</label>
+                        <textarea class="form-control" id="Message" name="message" rows="5" placeholder="Enter Message..." required></textarea>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" name="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Send</button>
+                    </div>            
+                </form>
 			</div>
 		</section>
 
@@ -207,4 +259,16 @@
 			</div>
 			<div class="col-xs-12">
 				<div class="btn-lrg submit-btn">Send Message</div>
-			</div>-->
+			</div>	-->
+
+			<!-- <div class="col-1">
+					<a href="mailto:zjaksu@gmail.com"><i class="far fa-envelope"> zjaksu@gmail.com</i></a>
+				</div>
+				
+				<div class="col-1">
+					<a href="https://github.com/westwashin"><i class="fab fa-github"> westwashin</i></a>
+				</div>
+				
+				<div class="col-1">
+					<a href="https://www.linkedin.com/in/syuja-zhafran-777b331ba/"><i class="fab fa-linkedin"> Syuja Zhafran</i></a>
+				</div> -->
